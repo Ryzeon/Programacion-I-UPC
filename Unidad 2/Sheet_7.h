b5f21d264c0e2377bf6c3cecb6d2b3616ca7e27c
Ryzeon <alex.ramon2004@gmail.com>
2022-04-29 00:54:12 -0500

    init sheet 7

---
+++ Unidad 2/Sheet_7.h
@@ -0,0 +1,19 @@
+//
+// Created by ryzeon on 28/04/22.
+//
+
+#ifndef EJERCICIOS_SHEET_7_H
+#define EJERCICIOS_SHEET_7_H
+
+
+class Sheet_7 {
+public:
+    Sheet_7(int choice) {
+        switch (choice) {
+
+        }
+    }
+};
+
+
+#endif //EJERCICIOS_SHEET_7_H
