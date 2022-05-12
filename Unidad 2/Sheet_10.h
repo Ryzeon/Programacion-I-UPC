fec0c169c78b450318ef87e6b8ef93fac0a848a6
Ryzeon <alex.ramon2004@gmail.com>
2022-05-12 12:24:40 -0500

    Done sheet 10 (hoja 5)

--- Unidad 2/Sheet_10.h
+++ Unidad 2/Sheet_10.h
@@ -163,115 +163,202 @@ private:
         }
     }
 
+
     void exercise_7() {
-        int n = utils::ask<int>("Ingrese N: ", ([](int v) { return v > 0 && v < 10; }),
+        int n = 5;
+        int a = utils::ask<int>("Ingrese N: ", ([](int v) { return v > 0 && v < 10; }),
                                 "El valor debe ser mayor a 0 y menor que 10");
+
         for (int i = 0; i < n; i++) {
-            string row = "";
-            for (int j = 0; j < n; j++) {
-                if (i == j || i == n - 1 || j == 0) {
-                    row += "* ";
-                } else {
-                    row += "  ";
+            for (int z = 0; z < a; z++) {
+                if (z != 0) {
+                    cout << "          ";
+                }
+                for (int j = 0; j < n; j++) {
+                    if (i == j || i == n - 1 || j == 0) {
+                        cout << "* ";
+                    } else {
+                        cout << "  ";
+                    }
                 }
             }
-            string sb = row;
-            sb += add_spaces(10);
-            sb += row;
-            sb += add_spaces(10);
-            sb += row;
-            cout << setw(50);
-            cout << sb << endl;
+            cout << endl;
         }
-    }
-
-    string add_spaces(int spaces) {
-        string output = "";
-        for (int i = 0; i < spaces; i++) {
-            output += " ";
-        }
-        return output;
     }
 
     void exercise_8() {
         int n = utils::ask<int>("Ingrese N: ", ([](int v) { return v > 0 && v < 10; }),
                                 "El valor debe ser mayor a 0 y menor que 10");
-        string *rows = new string[n];
         int centerIndex = 1;
         for (int i = 1; i <= n; ++i) {
-            string row;
-            // cout << "NUMERO DE FILA " << i << endl;
+            for (int ga = 0; ga < (n - centerIndex); ga++) {
+                cout << " ";
+            }
             for (int j = 1; j <= i; ++j) {
-                // cout << "NUMERO DE  J " << j << endl;
-                if(j  == centerIndex) {
-                 //   cout << "----------- COMIENZA REVERSA CUANDO J ES " << j << " ------------" << endl;
+                if (j == centerIndex) {
                     for (int k = j; k >= 1; --k) {
-                   //     cout << "VALOR DE K" <<  k << endl;
-                        row +=to_string(k);
+                        cout << k;
                     }
-                //    cout << "-----------  TERMINA REVERSA -----------" << endl;
                     break;
                 } else {
-                    row +=to_string(j);
+                    cout << j;
                 }
             }
-            rows[i - 1] = add_spaces(n - centerIndex) + row;
+            cout << endl;
             centerIndex++;
-        }
-        for (int i = 0; i < n; ++i) {
-            cout << rows[i] << endl;
         }
     }
 
     void exercise_9() {
         int n = utils::ask<int>("Ingrese N: ", ([](int v) { return v > 0 && v < 10; }),
-                                "El valor debe ser mayor a 0 y menor que 11");
-        string *rows = new string[  (n * 2) -1];
+                                "El valor debe ser mayor a 0 y menor que 10");
+
         int centerIndex = 1;
-        for (int i = 1; i <= n; ++i) {
-            string row;
-            // cout << "NUMERO DE FILA " << i << endl;
-            for (int j = 1; j <= i; ++j) {
-                // cout << "NUMERO DE  J " << j << endl;
-                if(j  == centerIndex) {
-                    //   cout << "----------- COMIENZA REVERSA CUANDO J ES " << j << " ------------" << endl;
-                    for (int k = j; k >= 1; --k) {
-                        //     cout << "VALOR DE K" <<  k << endl;
-                        row +=to_string(k);
+        for (int i = 1; i <= (n * 2) - 1; ++i) {
+            for (int ga = 0; ga < (n - centerIndex); ga++) {
+                cout << " ";
+            }
+            int numberToLoop = i;
+            if (i > n) {
+                numberToLoop = (n * 2) - i;
+            }
+            for (int j = 1; j <= numberToLoop; ++j) {
+                if (j == centerIndex) {
+                    for (int k = 0; k < j; k++) {
+                        cout << (j + k);
                     }
-                    //    cout << "-----------  TERMINA REVERSA -----------" << endl;
                     break;
                 } else {
-                    row +=to_string(j);
+                    cout << j;
                 }
             }
-            rows[i - 1] = add_spaces(n - centerIndex) + row;
-            centerIndex++;
-        }
-        for (int i = (n * 2) -2; i >= n; ++i) {
-            string row = rows[i];
-            row = row.substr(0, row.length() - 1);
-            cout << rows->size() << " SIZE" << endl;
-            cout << rows->length() << " LEGHT" << endl;
-            rows[i] = row;
-            cout << rows->size() << " SIZE" << endl;
-            cout << rows->length() << " LEGHT" << endl;
-        }
-        for (int i = 0; i < rows->length(); ++i) {
-            cout << rows[i] << endl;
+            cout << endl;
+            if ((i + 1) > n) {
+                centerIndex--;
+            } else {
+                centerIndex++;
+            }
         }
     }
 
     void exercise_10() {
+        int n = utils::ask<int>("Ingrese N: ", ([](int v) { return v > +2 && v <= 10; }),
+                                "El valor debe ser mayor a 2 y menor que 10");
+        for (int i = 0; i < n; i++) {
+            int repeat = 0;
+            int spaces = (n * 2 - 2);
+            spaces -= i;
+            // - 2 para quitarle el ultimo y el primero
+            int numberToLoop = (n - i);
+            for (int j = n; j >= numberToLoop; j--) {
+                cout << j;
+                repeat++;
+                spaces--;
+                if (j == 1) {
+                    // reset
+                    repeat = 0;
+                    // reset
+                    spaces = 0;
+                    for (int k = 2; k <= n; k++) {
+                        cout << k;
+                    }
+                }
+            }
+            for (int z = 0; z < spaces; z++) {
+                cout << " ";
+            }
+            for (int k = repeat; k > 0; k--) {
+                cout << (n - k + 1);
+            }
+            cout << endl;
+        }
 
+        // lo mismo pero invertido xd
+        for (int i = (n - 2); i >= 0; i--) {
+            int repeat = 0;
+            int spaces = (n * 2 - 2);
+            spaces -= i;
+            // - 2 para quitarle el ultimo y el primero
+            int numberToLoop = (n - i);
+            for (int j = n; j >= numberToLoop; j--) {
+                cout << j;
+                repeat++;
+                spaces--;
+            }
+            for (int z = 0; z < spaces; z++) {
+                cout << " ";
+            }
+            for (int k = repeat; k > 0; k--) {
+                cout << (n - k + 1);
+            }
+            cout << endl;
+        }
     }
 
     void exercise_11() {
-
+        int n = utils::ask<int>("Ingrese N: ", ([](int v) { return v > 0 && v < 10; }),
+                                "El valor debe ser mayor a 0 y menor que 10");
+        int rowReversed = n;
+        int columnReversed = n;
+        for (int row = 1; row <= (n * 2 - 1); row++) {
+            int reversed = n;
+            for (int column = 1; column <= (n * 2 - 1); column++) {
+//                if (row == 1 || column == 1 || row == (n * 2 - 1) || column == (n * 2  - 1)) {
+                if (row == 1 || row == (n * 2 - 1) || column == 1 || column == (n * 2 - 1)) {
+                    int toPrint = row == 1 || row == (n * 2 - 1) ? column : row;
+                    if (toPrint > n) {
+                        if (toPrint == column) {
+                            reversed--;
+                            toPrint = reversed;
+                        } else {
+                            if (column == 1) {
+                                columnReversed--;
+                                toPrint = columnReversed;
+                            } else {
+                                rowReversed--;
+                                toPrint = rowReversed;
+                            }
+                        }
+                    }
+                    cout << " " << toPrint;
+                } else {
+                    cout << "  ";
+                }
+            }
+            cout << endl;
+        }
     }
 
     void exercise_12() {
+        int n = utils::ask<int>("Ingrese N: ", ([](int v) { return v > 0 && v < 10; }),
+                                "El valor debe ser mayor a 0 y menor que 10");
 
+        int centerIndex = 1;
+        for (int i = 1; i <= (n * 2) - 1; ++i) {
+            for (int ga = 0; ga < (n - centerIndex); ga++) {
+                cout << " ";
+            }
+            int numberToLoop = i;
+            if (i > n) {
+                numberToLoop = (n * 2) - i;
+            }
+            for (int j = 1; j <= numberToLoop; ++j) {
+                if (j == centerIndex) {
+                    for (int k = 0; k < j; k++) {
+                        cout << (char) ((j + k) + 64);
+                    }
+                    break;
+                } else {
+                    cout << (char) (j + 64);
+                }
+            }
+            cout << endl;
+            if ((i + 1) > n) {
+                centerIndex--;
+            } else {
+                centerIndex++;
+            }
+        }
     }
 };
 
